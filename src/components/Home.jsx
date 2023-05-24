54cec141f90616133c381f5c8eb19d51273c967a
John-13-7 <galberto2573@googlemail.com>
2023-05-23 20:36:47 -0700

    refactoring

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <li>
+        <Link to="/BlackJack"> BlackJack </Link>
+      </li>
+      <li>
+        <Link to="/Roulette"> Roulette </Link>
+      </li>
+      <li>
+        <Link to="/Slots"> Slots </Link>
+      </li>
+      <li>
+        <Link to="/Scratchers"> Scratchers </Link>
+      </li>
+      <li>
+        <Link to="/Lottery"> Lottery </Link>
+      </li>
+    </div>
   );
-};
+}
 
 export default Home;
