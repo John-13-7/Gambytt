0c2a1d05df31478dc54edb443debe3e96b48440c
John-13-7 <galberto2573@googlemail.com>
2023-07-11 02:47:13 -0400

    Can now update the users balance in backend

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 function Logout() {
-  return <div>Logout</div>;
+  const [updateBalance, setUpdateBalance] = useState(420);
+  const handleBalance = () => {
+    axios.patch(
+      "http://localhost:5000/update_balance",
+      { update: updateBalance },
+      { withCredentials: true }
+    );
+  };
+
+  const handleLogout = () => {
+    axios
+      .post("http://localhost:5000/logout", {}, { withCredentials: true })
+      .catch((error) => {
+        console.log(error);
+        alert("An error occurred during logout."); // alert user on error
+      });
+  };
+  return (
+    <div>
+      <button onClick={handleBalance}>
+        Click to add 420$ to your account!
+      </button>
+      <button onClick={handleLogout}>Press to erase everything handsome</button>
+    </div>
+  );
 }
 
 export default Logout;
